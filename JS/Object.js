434334d541f3d84c98bc2c8b6df3d3144bb84523
saim1sh <saim.sh0100@gmail.com>
2026-03-07 23:59:24 +0600

    nested

--- JS/Object.js
+++ JS/Object.js
@@ -14,3 +14,20 @@ const info ={
 // Accessing object properties
 console.log(info.name);
 console.log(info.sum(5,6))
+
+
+//nested object
+const person = {
+   name: "Saim",
+   age: 22,
+   address: {
+      city: "Karachi",
+      country: "Pakistan",
+      home :{
+         city: "Karachi",
+         country: "Pakistan"
+      }
+   }
+}
+
+const {name, address:{home:{country}}} = person;
